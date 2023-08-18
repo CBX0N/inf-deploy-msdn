0d965053485c72c5f3be19692ad7a5864cf1fe21
Charlie Boon <88788571+CBX0N@users.noreply.github.com>
2023-08-18 20:46:03 +0100

    Add files via upload

---
+++ manage-tfstate.ps1
@@ -0,0 +1,19 @@
+[CmdletBinding()]
+param (
+  [Parameter()][string]$tf_rg_name = "rg-tfstate-cbx-dev-uks-001",
+  [Parameter()][string]$tf_sa_name = "satfstatecbxdevuks001",
+  [Parameter()][string]$tf_container_name = "tfstate",
+  [Parameter()][string]$location = "uksouth",
+  [Parameter()][validateSet("create","destroy",IgnoreCase = $true)]$action = "create"
+)
+
+if ("create" -eq $action){
+az group create --location $location --resource-group $tf_rg_name 
+az storage account create --name $tf_sa_name --resource-group $tf_rg_name --location $location --sku Standard_LRS 
+az storage container create --name $tf_container_name --account-name $tf_sa_name 
+}
+
+if ("destroy" -eq $action){
+az storage container delete --name $tf_container_name --account-name $tf_sa_name 
+az storage account delete --name $tf_sa_name --resource-group $tf_rg_name --yes
+az group delete --resource-group $tf_rg_name --yes }
